1a0ed4b0a8a41a14a68e1e5f62ac67a2a8920999
zaqbez39me <korolov_k@mail.ru>
2024-04-10 20:41:45 +0300

    Added main endpoints with logic

--- random_generator/web/api/router.py
+++ random_generator/web/api/router.py
@@ -1,9 +1,8 @@
 from fastapi.routing import APIRouter
 
-from random_generator.web.api import docs, echo, monitoring, redis
+from random_generator.web.api import docs, monitoring, random
 
 api_router = APIRouter()
 api_router.include_router(monitoring.router)
 api_router.include_router(docs.router)
-api_router.include_router(echo.router, prefix="/echo", tags=["echo"])
-api_router.include_router(redis.router, prefix="/redis", tags=["redis"])
+api_router.include_router(random.router)
