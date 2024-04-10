56c6d40d45e434717f2776dfe2cc3e0853c303f8
IlnurHA <53339563+IlnurHA@users.noreply.github.com>
2024-04-10 19:25:52 +0300

    Flake8 and black fix

--- random_generator_front_end/streamlit_app.py
+++ random_generator_front_end/streamlit_app.py
@@ -1,53 +1,76 @@
+from typing import List, Union
+
 import streamlit as st
+import ujson
 from config import settings
 from httpx import Client
-from ujson import loads
-from typing import Union, List
 
 
 def fetch_from_backend(client: Client, selected_source: str):
-    data = {
-        'source': selected_source
-    }
+    """Fetches number from backend from specified source.
+    Requires for response to be in format {"number": ...}.
+    Shows it in streamlit
 
+    Args:
+        client (Client): httpx client with specified base url
+        selected_source (str): source to fetch from
+    Returns:
+        None
+    """
+    data = {"source": selected_source}
     result = client.post("/api/random", json=data)
-
     if result.status_code == 200:
-        result_json: dict = loads(result.text)
-
+        result_json: dict = ujson.loads(result.text)
         if not isinstance(result_json, dict) or "number" not in result_json.keys():
             st.write("Error: Invalid JSON from the server")
         else:
-            st.write(result_json['number'])
+            st.write(result_json["number"])
     else:
         st.write(f"Error: status code {result.status_code}")
 
 
 def get_sources(client: Client) -> Union[List[str], None]:
-    sources_json: dict = loads(client.get("/api/random").text)
+    """Gets sources from backend.
+    Requires for response to be in format {"sources": [...]}.
 
+
+    Args:
+        client (httpx.Client): Client with specified base url
+    Returns:
+        None: if error is found, None is returned
+        list[str]: list of sources
+    """
+    sources_json: dict = ujson.loads(client.get("/api/random").text)
     if not isinstance(sources_json, dict):
         return None
-
     if "sources" not in sources_json.keys():
         return None
-
-    return list(sources_json['sources'])
+    return list(sources_json["sources"])
 
 
 def random_generator(client: Client):
+    """Streamlit app.
+
+    Args:
+        client (httpx.Client): Client with specified base url
+    Returns:
+        None
+    """
+
     sources = get_sources(client)
 
     if sources is not None:
-        x = st.selectbox("Select source to generate a random number", sources)
+        source = st.selectbox("Select source to generate a random number", sources)
 
-        st.button('Generate a random number',
-                  on_click=lambda: fetch_from_backend(client, x))
+        st.button(
+            "Generate a random number",
+            on_click=lambda: fetch_from_backend(client, source),
+        )
     else:
         st.write("Error: Failed fetching sources from the server")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     st.title("Random Generator")
     http_client = Client(base_url=settings.BACKEND_URL, timeout=3)
     random_generator(http_client)
